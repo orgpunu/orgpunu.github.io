c74a320be182180ea293010059caea26fa00d85e
orgpunu <orgpunu@bk.ru>
2025-07-17 18:10:50 -0400

    Update CTEC.js

--- 019381/bd/CTEC.js
+++ 019381/bd/CTEC.js
@@ -5,7 +5,7 @@ const CTEC = [
     categoria: "Generalista",
     idioma: "Español",
     logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/TVN_Chile_logo_2016.svg/1200px-TVN_Chile_logo_2016.svg.png",
-    link: "mono.m3u8?remote=38.61.157.63&token=d57ce20de7c27c…87da348c75c30817b9b0fbe7e2d-1752811255-1752789655"
+    link: "https://live2.saohgdasregions.fun:9091/42_/tracks-v1a1/mono.m3u8?remote=38.61.157.63&token=d57ce20de7c27c8998c8cc8ab57a1fb1cb1a26aa-cf89587da348c75c30817b9b0fbe7e2d-1752811255-1752789655"
   },
   {
     id: "002",
